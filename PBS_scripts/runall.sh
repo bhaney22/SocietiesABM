2d66ecdf7880a7efd5bcd2f812896dad73c85df0
Becky Haney <bhaney@calvin.edu>
2017-10-14 17:38:13 +0000

    saveDevRecipes and saveIOmatrix for last day.

--- PBS_scripts/runall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#      Calls the q.sh program that executes the qsub commands. 
-#      example: "./q NoDev_4_4 30" [will use the Configs/NoDev_4_4.conf file and submit 30 qsubs]
-#
-# the .pbs scripts are set up to receive the name of the config file and # runs as arguments
-
-./q T1_4_4 2
-
