f435ed63aa24c753525942a293a86f696c48b6cf
Becky Roselius Haney <bhaney@calvin.edu>
2018-05-30 16:15:28 -0400

    Trying to fix call to prodstats

--- utils.cpp
+++ utils.cpp
@@ -1202,8 +1202,8 @@ void Utils::saveDeviceRecipes()
 void Utils::saveUseMatrix()
 {	vector< vector< vector<int> > > devicesMadeByRes = glob.productionStats->getDevicesMadeByRes();
     vector<vector<int> > resGatheredByRes = glob.productionStats->getResGatheredByRes();
-    vector< vector<vector<vector<int> > > > TimeSpentGatheringWithDeviceByRes = glob.productionStats->getTimeSpentGatheringWithDeviceByRes;  //BRH NEW variable 05.29.2018
-    vector< vector<vector<int> > > TimeSpentGatheringWithoutDeviceByRes = glob.productionStats->getTimeSpentGatheringWithoutDeviceByRes;   
+    vector< vector<vector<vector<double> > > > TimeSpentGatheringWithDeviceByRes = glob.productionStats->getTimeSpentGatheringWithDeviceByRes;  //BRH NEW variable 05.29.2018
+    vector< vector<vector<double> > > TimeSpentGatheringWithoutDeviceByRes = glob.productionStats->getTimeSpentGatheringWithoutDeviceByRes;   
     int temp_in_device=0;
 	double num_of_that_device_made;
     double tempTimeSpentGatheringWithDeviceByRes;
