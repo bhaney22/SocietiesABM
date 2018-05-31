c9e90ae5fe9754bca4ef862b302ae34f04d76a2a
Becky Roselius Haney <bhaney@calvin.edu>
2018-05-30 23:08:25 -0400

    Trying to fix call to prodstats

--- utils.cpp
+++ utils.cpp
@@ -1269,7 +1269,7 @@ void Utils::saveUseMatrix()
         } else {
                 double sumTimeSpentGatheringWithDeviceByRes = 0.0;
                 for (int aId = 0; aId < glob.NUM_AGENTS; aId++) { 
-                    tempTimeSpentGatheringWithDeviceByRes = int timeSpentGatheringWithDeviceByRes[TOOL][resId][glob.currentDay]; 
+                    tempTimeSpentGatheringWithDeviceByRes =  timeSpentGatheringWithDeviceByRes[TOOL][resId][glob.currentDay]; 
                         sumTimeSpentGatheringWithDeviceByRes += tempTimeSpentGatheringWithDeviceByRes;
                 }
                 file << "," << sumTimeSpentGatheringWithDeviceByRes;
