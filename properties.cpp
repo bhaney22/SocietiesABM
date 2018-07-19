8da282d1dbd5e39c06a4a98d5314b584716c9011
Becky Haney <bhaney@calvin.edu>
2018-07-19 15:29:18 -0400

    revert to May 31 version

--- properties.cpp
+++ properties.cpp
@@ -61,8 +61,7 @@ ResProperties::ResProperties()
     resSetAside = 0;
     unitsGatheredToday = 0;
     unitsGatheredEndWork = 0;
-    //JYC: temporary comment		2018.07.16
-//    timeSpentGatheringWithoutDeviceToday = 0;
+    timeSpentGatheringWithoutDeviceToday = 0;
 
     beforeWorkHeld = 0;
     boughtEndWork = 0;
@@ -79,7 +78,7 @@ void ResProperties::calcResEfforts()
 {
     assert(resEfforts.empty());
     resEfforts.clear();
-    for (double potentialExp = 0; potentialExp < double(maxResExperience); potentialExp++) {
+    for (int potentialExp = 0; potentialExp < int(maxResExperience); potentialExp++) {
         resEfforts.push_back(maxResEffort -
                 (maxResEffort - minResEffort)
                 * exp(- sqrt(maxResEffort) * 2
@@ -145,7 +144,7 @@ DevProperties::DevProperties()
     gainOverDeviceLifeMemoryValid = false;
     costOfDeviceMemory = 0.0;
     costOfDeviceMemoryValid = false;
-    worstCaseConstructionMemory = make_pair(0.0, vector<double>(glob.NUM_RESOURCES, 0));
+    worstCaseConstructionMemory = make_pair(0.0, vector<int>(glob.NUM_RESOURCES, 0));
     worstCaseConstructionMemoryValid = false;
 
     devicesMadeToday = 0;
@@ -157,14 +156,14 @@ DevProperties::DevProperties()
     deviceMinutesDecayTotal = 0;
 
     devicePrices = vector<double>(glob.DEVICE_TRADE_MEMORY_LENGTH, 0.0);
-    devicesRecentlyMade = vector<double>(glob.DEVICE_PRODUCTION_MEMORY_LENGTH, 0);																											//JYC: changed to type double
+    devicesRecentlyMade = vector<int>(glob.DEVICE_PRODUCTION_MEMORY_LENGTH, 0);
 }
 
 void DevProperties::calcDeviceEfforts()
 {
     assert(deviceEfforts.empty());
     deviceEfforts.clear();
-    for (int potentialExp = 0; potentialExp < int(maxDeviceExperience); potentialExp++) {																														//JYC: changed to type double
+    for (int potentialExp = 0; potentialExp < int(maxDeviceExperience); potentialExp++) {
         deviceEfforts.push_back( maxDeviceEffort -
                 (maxDeviceEffort - minDeviceEffort)
                 * exp(-maxDeviceEffort * 2
