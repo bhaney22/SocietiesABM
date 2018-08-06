91ce2cd88f3006736a15e291f5fdec72eb58e365
Becky Haney <bhaney@calvin.edu>
2018-08-06 14:09:45 -0400

    run all itest scripts

---
+++ runITEST_batch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/SocietiesABM/
+
+# ./runITEST_trade.sh 300 100
+./runITEST_experience.sh 300 100
+./runITEST_devices.sh 300 100
+./runITEST_effort.sh 300 100
+./runITEST_epsilons.sh 300 100
